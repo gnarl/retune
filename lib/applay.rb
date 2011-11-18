79dd535cbc2efba950692c0e263ca19d530b5a36
Chuck Fouts <github@nugnug.net>
2011-11-17 20:29:48 -0500

    Added next, previous, and then reordered methods.

--- lib/applay.rb
+++ lib/applay.rb
@@ -11,21 +11,29 @@ class ItunesAppScript
     @lib = @itu.playlists["Library"]
   end
   
-  def songs_by_artist(name)
-    @lib.tracks[@whose.artist.eq(name)].name.get
-  end
-
   def artists
     artists = @lib.tracks.artist.get
     artists.uniq
+  end
+
+  def next 
+    @itu.next_track
+  end
+
+  def play 
+    @itu.play
   end
 
   def play_song(song_name)
     @itu.play(@lib.tracks[song_name])
   end
 
-  def play 
-    @itu.play
+  def previous 
+    @itu.previous_track
+  end
+
+  def songs_by_artist(name)
+    @lib.tracks[@whose.artist.eq(name)].name.get
   end
 
   def stop
--- retune.rb
+++ retune.rb
@@ -9,13 +9,7 @@ before do
   @iTunesAppScript = ItunesAppScript.new unless @ItunesAppScript 
 end
 
-get '/music/play' do
-  @iTunesAppScript.play
-end
-
-get '/music/song/:song' do
-  @iTunesAppScript.play_song(params[:song])
-end
+#TODO figure out how to match routes to appScript method names
 
 get '/music/artists' do
   content_type :json
@@ -23,10 +17,26 @@ get '/music/artists' do
   list.sort.to_json
 end
 
+get '/music/next' do
+  @iTunesAppScript.next
+end
+
+get '/music/play' do
+  @iTunesAppScript.play
+end
+
+get '/music/previous' do
+  @iTunesAppScript.previous
+end
+
 get '/music/show/:artist' do
-  list = @iTunesAppScript.songs_by_artist(params[:artist])
   content_type :json
+  list = @iTunesAppScript.songs_by_artist(params[:artist])
   list.to_json
+end
+
+get '/music/song/:song' do
+  @iTunesAppScript.play_song(params[:song])
 end
 
 get '/music/stop' do
