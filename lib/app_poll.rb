04362ab36d6efe30053a21af8bfad11bc8bdee41
Chuck Fouts <github@nugnug.net>
2011-11-26 12:43:10 -0500

    Moved method that handles polling thread to a separate class.

---
+++ lib/app_poll.rb
@@ -0,0 +1,48 @@
+require 'appscript'
+
+include Appscript
+
+#Add testing
+
+class AppPoll
+  POLL_SLEEP = 5
+
+  def initialize(itunes_app, queue_name='queue')
+    @itu = itunes_app 
+    @queue = @itu.playlists[queue_name]
+  end
+
+  def start_poll
+    puts "starting app polling thread"
+    unless @poll_thread
+      @poll_thread = Thread.start do
+      i = 1
+        while(true)
+          puts "polling #{i+=1}"
+          begin
+            remove_previous_tracks 
+          rescue
+            puts $! 
+          end
+          sleep(POLL_SLEEP)
+        end
+      end
+    end
+    @poll_thread
+  end
+
+  def remove_previous_tracks
+    if @queue.exists && !queue_empty? 
+        #puts  @itu.current_track.exists
+        trax = @queue.tracks.get
+        index = trax.index(@itu.current_track.get)
+        if index > 0
+          (0..(index-1)).each do |i|
+            puts "removed #{trax[i].name.get} from queue"
+            @queue.tracks[trax[i].name.get].delete
+          end
+        end
+      end
+  end
+end
+
---
+++ test/app_poll_test.rb
@@ -0,0 +1,15 @@
+require 'test/unit'
+require 'app_poll'
+
+class AppPollTest < Test::Unit::TestCase
+
+  TEST_QUEUE = 'test_queue'
+
+#Add Testing
+  # mock out applay
+
+#Rename applay
+#commit
+
+
+end
